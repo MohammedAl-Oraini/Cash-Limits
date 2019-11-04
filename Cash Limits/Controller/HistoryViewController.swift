963c809a6963d9f5315cbc3da2b7759780fc52fc
Mohammad Al-Oraini <hp-mmm@hotmail.com>
2019-11-04 17:14:23 +0300

    adding history table view

--- Cash Limits/Controller/HistoryViewController.swift
+++ Cash Limits/Controller/HistoryViewController.swift
@@ -8,13 +8,25 @@
 
 import UIKit
 
-class HistoryViewController: UIViewController {
+class HistoryViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "HistoryCell", for: indexPath)
+        cell.backgroundColor = .red
+        
+        
+        
+        return cell
+    }
 
 }
 
---
+++ Cash Limits/View/HistoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HistoryTableViewCell.swift
+//  Cash Limits
+//
+//  Created by Mohammad Al-Oraini on 04/11/2019.
+//  Copyright © 2019 Mohammad Al-Oraini. All rights reserved.
+//
+
+import UIKit
+
+class HistoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
